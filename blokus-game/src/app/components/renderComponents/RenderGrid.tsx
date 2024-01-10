459606229764f48b6827c1e65bb54fcf8b86a05c
Miran-Mirantee <killerm11805@hotmail.com>
2024-01-11 01:49:19 +0700

    Added pieces object

--- blokus-game/src/app/components/renderComponents/RenderGrid.tsx
+++ blokus-game/src/app/components/renderComponents/RenderGrid.tsx
@@ -4,29 +4,85 @@ import CreateGrid from "../logicComponents/CreateGrid";
 import { Matrix } from "ts-matrix";
 import { useState } from "react";
 
+type CoordinatesArray = Array<[number, number, number]>;
+
 export const RenderGrid = () => {
-  const grid = CreateGrid(20, 20);
-  const board = new Matrix(20, 20);
-  // console.log(board.values);
+  const matrix = new Matrix(20, 20);
+  const [board, setBoard] = useState(matrix.values);
+
+  const updateBoardValues = (
+    x: number,
+    y: number,
+    value: number,
+    callback: (x: number, y: number, value: number) => CoordinatesArray
+  ) => {
+    let updatedBoard = [...board]; // Assuming board is your original array
+    const coordinates: CoordinatesArray = callback(x, y, value);
+
+    coordinates.forEach(([x, y, newValue]: [number, number, number]) => {
+      updatedBoard = updatedBoard.map((row, cIndex) => {
+        if (cIndex === y) {
+          return row.map((value, rIndex) => (rIndex === x ? newValue : value));
+        }
+        return row;
+      });
+    });
+
+    return updatedBoard;
+  };
+  // const updateBoardValues = (coordinates: CoordinatesArray) => {
+  //   let updatedBoard = [...board]; // Assuming board is your original array
+
+  //   coordinates.forEach(([x, y, newValue]: [number, number, number]) => {
+  //     updatedBoard = updatedBoard.map((row, cIndex) => {
+  //       if (cIndex === y) {
+  //         return row.map((value, rIndex) => (rIndex === x ? newValue : value));
+  //       }
+  //       return row;
+  //     });
+  //   });
+  //   return updatedBoard;
+  // };
+
+  const pieces = {
+    oneSquare: (x: number, y: number, value: number) => {
+      return [
+        [x, y, value],
+        [x - 1, y, value],
+        [x + 1, y, value],
+        [x, y - 1, value],
+        [x, y + 1, value],
+      ] as CoordinatesArray;
+    },
+  };
+
   return (
     <div className="flex flex-col bg-red-100 shadow-none shadow-rose-700">
-      {board.values.map((column: any, columnIndex) => {
+      {board.map((column: any, columnIndex) => {
         return (
           <div
             key={columnIndex}
             className="flex border-solid border-red-400 first:border-t border-b "
           >
-            {column.map((tile: any, tileIndex: any) => {
+            {column.map((tile: any, rowIndex: any) => {
               return (
                 <div
-                  key={tileIndex}
-                  className={`border-solid border-red-400 first:border-l border-r h-10 w-10 hover:bg-red-300`}
-                  onMouseEnter={() => {
-                    console.log("Mouse entered");
+                  key={rowIndex}
+                  className={`border-solid border-red-400 first:border-l border-r h-10 w-10 hover:bg-yellow-300 ${
+                    tile == 1 ? "bg-red-300" : ""
+                  }`}
+                  onClick={() => {
+                    setBoard(
+                      updateBoardValues(
+                        rowIndex,
+                        columnIndex,
+                        1,
+                        pieces.oneSquare
+                      )
+                    );
                   }}
-                  onMouseLeave={() => {
-                    console.log("Mouse left");
-                  }}
+                  onMouseEnter={() => {}}
+                  onMouseLeave={() => {}}
                 ></div>
               );
             })}
